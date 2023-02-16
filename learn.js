818ac6e9f3dce1636b1f9300874344fc43760b4c
DeIshara <sandaruwanishara96@gmail.com>
2023-02-16 15:07:39 +0530

    resolve last part calculation errors

--- learn.js
+++ learn.js
@@ -1,10 +1,10 @@
 const prompt = require('prompt-sync')();
 
 let peoples = parseInt(prompt("Hello enter number of peoples in your order: "));
-let peices = parseInt(prompt("Hello enter number of peices do you want: "));
+let peices = parseInt(prompt("Hello enter number of pieces do you want: "));
+// let size = parseInt(prompt("Enter you need pizza size (5inch,7inch,10inch,15inch,24inch)- enter number only: "));
 
 function CustomizePizza(NumPeoples, NumPieces) {
-    let number_of_slices_for_people = NumPieces/NumPeoples;
     console.log(`I think you need ${NumPieces} pieces because you have ${NumPeoples} peoples`);
     if(NumPieces > 0 && NumPieces < 5) {
         console.log( `You can buy this has 4 pieces \n 5 inch size \n Lowest Price is according to your need $3`);
@@ -17,8 +17,9 @@ function CustomizePizza(NumPeoples, NumPieces) {
     }else if (NumPieces > 15 && NumPieces < 25){
         console.log( `You can buy this has 24 pieces \n 24 inch size \n Lowest Price is according to your need $18 `);
     }else if (NumPieces > 24){
-        let total_price = number_of_slices_for_people * 18;
-        console.log(` For one people get these pieses: ${number_of_slices_for_people} and your price is $${total_price}` );
+        let num_of_24inch_pizzas = NumPieces/24;
+        let total_price = num_of_24inch_pizzas * 18;
+        console.log(` For group of people get 24 inch pizzas: ${num_of_24inch_pizzas} and your Total price is $${total_price}` );
     }
     
 }
